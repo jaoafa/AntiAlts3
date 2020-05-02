4c4b2eee187e789f31f5c05be3b4fb684f71049b
Tomachi (ICHIGO) <lego@aol.jp>
2020-05-02 18:36:21 +0900

    change: バージョン表記変更

--- InsertVersion.php
+++ InsertVersion.php
@@ -1,8 +1,12 @@
 <?php
 // ビルド時にバージョンを自動で設定する
 date_default_timezone_set("Asia/Tokyo");
-define("YmlLocation", __DIR__ . "/src/main/resources/plugin.yml");
-define("Version", date("Y.m.d_H.i"));
+define("YmlLocation", __DIR__ . "/src/main/resources/plugin.yml");if (file_exists(__DIR__ . "/src/main/resources/COMMIT")) {
+    $COMMIT = "_" . mb_substr(file_get_contents(__DIR__ . "/src/main/resources/COMMIT"), 0, 7);
+} else {
+    $COMMIT = "";
+}
+define("Version", date("Y.m.d_H.i") . $COMMIT);
 
 if(!file_exists(YmlLocation)){
     echo "Yml not found.\n";
